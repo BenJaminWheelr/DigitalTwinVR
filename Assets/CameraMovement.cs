cb4a501f1b77761c9e016e1f767aa8eea2680e7a
Benjamin Wheeler <benjaminwheelr@gmail.com>
2025-10-26 12:47:00 -0600

    added finished library model, and added colliders for the model

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,45 +1,39 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class CameraMovement : MonoBehaviour
 {
-    private float sped = 20.0f;
+    public float speed = 5.0f;
+    public float rotSens = 3.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    private CharacterController controller;
 
-    private float rotSens = 3.0f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-    private Vector3 room1TeleportLocation = new Vector3(0, 3, 0);
-    private Vector3 room2TeleportLocation = new Vector3(0, 3, -25.22f);
-
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 position = new Vector3(0, 0 ,0);
+        // Rotation
         yaw += Input.GetAxis("Mouse X") * rotSens;
         pitch -= Input.GetAxis("Mouse Y") * rotSens;
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
-        // --- WASD Movement ---
-        float moveX = Input.GetAxis("Horizontal"); // A/D
-        float moveZ = Input.GetAxis("Vertical");   // W/S
-
+        // Movement input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
 
-        // --- Teleport when pressing 1 ---
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            position.y += sped * Time.deltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            position.y -= sped * Time.deltaTime;
-        }
-        transform.position += position + move * sped * Time.deltaTime;
-        
+        // Vertical (jump/fly) movement
+        if (Input.GetKey(KeyCode.Space))
+            move.y += 1;
+        if (Input.GetKey(KeyCode.LeftControl))
+            move.y -= 1;
 
+        // Apply movement with collision
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
